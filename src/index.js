440da5db73ba13a8281ca29911374ebdb08cd076
leigh966 <leigh966@hotmail.com>
2022-07-25 14:51:19 +0100

    Created a basic front-end template for a jackbox-style game

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+/**
+ *
+ * @param {Response} response
+ */
+function getJoinResponse(response) {
+  if (response.status == 201) {
+    alert("Successfully joined game!");
+  } else if (response.status == 409) {
+    alert("This username is already taken!");
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function join() {
+  const username = document.getElementById("txtUsername").value;
+  const ip = document.getElementById("txtIp").value;
+  const url = `http://${ip}:9669/join?username=${username}`;
+  fetch(url)
+    .then((data) => getJoinResponse(data))
+    .catch((err) => alert(err));
+}
+
+export default function App() {
+  return (
+    <>
+      <label>Enter the ip of the host</label>
+      <input id="txtIp" type="text" placeholder="192.168.?.?" />
+      <br />
+
+      <label>Pick a username</label>
+      <input id="txtUsername" type="text" placeholder="Username" />
+      <button type="button" onClick={join}>
+        Join
+      </button>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
